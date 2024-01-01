2b2c80823f9a887a2d48010cef99ae40a278d71d
divyaNichesolv <divya@nichesolv.com>
2024-01-01 20:50:23 +0530

    ep02

--- App.js
+++ App.js
@@ -1,19 +1,5 @@
-/* <div id="parent">
-  <div id="child1">
-    <h1>I m h1</h1>
-     <h1>I m h1</h1>
-  </div>
-  <div id="child1">
-    <h1>I m h1</h1>
-    <h1>I m h1</h1>
-  </div>
-</div> */
-
-// const heading = React.createElement(
-//   "h1",
-//   { id: "heading" },
-//   "hello reactjs from diffrent file"
-// );
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Welcome React
+
+# Parcel
+
+- Dev Build
+- created local server
+- HMR = Hot module replacement
+- File Watching Algorithm -written in c++
+- Caching - Faster builds
+- Image optimization
+- Minification
+- bundling
+- Compressing
+- Consisting Hashing
+- Code Splitting
+- Differential Bundling - support older browser
+- Diagonostic
+- Error Handling
+- Https
+- Tree Shakking - remove unused code
+- Different dev and prod bundles
