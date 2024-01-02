9088951588ab927526ea83918ab8d461593235b0
divyaNichesolv <divya@nichesolv.com>
2024-01-02 21:53:07 +0530

    ep03

--- App.js
+++ App.js
@@ -1,16 +1,10 @@
-import React from "react";
+import React, { createElement } from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I m h1"),
-    React.createElement("h1", {}, "I m h1"),
-  ]),
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I m h1"),
-    React.createElement("h1", {}, "I m h1"),
-  ]),
-]);
+// JSX
+// - its html like syntax not html inside javascript
+// - jsx => createElement by babel -js object =>htmlElement by babel and then rendered by render method
 
+const heading = <h1>Welcome to react</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
