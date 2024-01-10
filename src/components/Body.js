613b52b67a2bae198953fc71ff40b29eef513cc2
divyaNichesolv <divya@nichesolv.com>
2024-01-10 22:07:25 +0530

    epo6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,41 @@
 import ResturantCard from "./ResturantCard";
-import { restaurants } from "./../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [listOfRestuarants, setListOfRestuarants] = useState(restaurants);
+  const [listOfRestuarants, setListOfRestuarants] = useState();
+  const [filteredRestuarants, setFilteredRestuarants] = useState();
   const [searchInput, setSearchInput] = useState();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.898773&lng=77.5764094&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+
+    //OR
+
+    // const data = await fetch(
+    //   `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.898773&lng=77.5764094&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+    // );
+    // const dataNew = await data.json();
+
+    setFilteredRestuarants(
+      data?.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    );
+    setListOfRestuarants(
+      data?.data.cards[2].card.card.gridElements.infoWithStyle.restaurants
+    );
+  };
+
   return (
     <div className="body">
       <div className="search-container">
@@ -20,7 +51,7 @@ const Body = () => {
                 return restaurant;
               }
             });
-            setListOfRestuarants(topRestuarant);
+            setFilteredRestuarants(topRestuarant);
           }}
         >
           Search
@@ -28,7 +59,7 @@ const Body = () => {
       </div>
       <p>{searchInput}</p>
       <div className="card-container">
-        {listOfRestuarants?.map((restaurantData, index) => {
+        {filteredRestuarants?.map((restaurantData, index) => {
           return (
             <ResturantCard
               key={restaurantData.info.id}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/contants";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
+
+  const handleLogin = () => {
+    btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+  };
+
   return (
     <div className="header">
       <img src={LOGO_URL} className="logo"></img>
@@ -10,6 +17,9 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <li>
+            <button onClick={handleLogin}>{btnName}</button>
+          </li>
         </ul>
       </div>
     </div>
